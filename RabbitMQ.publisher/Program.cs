0a6acd40ffa9afeb9a5fd7693722e7dc3940422f
Songül Soytürk <90280719+SongulSYTRK@users.noreply.github.com>
2023-02-16 15:01:16 +0300

    Add project files.

---
+++ RabbitMQ.publisher/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory()
+{
+    Uri = new Uri("amqps://avlbkmsz:VczzGlHupMoF7ximnepb8fKOx4cieMn0@woodpecker.rmq.cloudamqp.com/avlbkmsz")
+};
+//factory.Uri = new Uri("amqps://avlbkmsz:VczzGlHupMoF7ximnepb8fKOx4cieMn0@woodpecker.rmq.cloudamqp.com/avlbkmsz");
+
+using var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+//exclure eğer true yaparsak saece bu kanal üzerinden ulaşabilirz. Ama biz başka processlerden ulaşmmak için false deriz
+
+
+channel.QueueDeclare("hello-queue", true, false,false);//exchange kullanmadan direkt kuyruk declare etmiştik 
+
+Enumerable.Range(1, 50).ToList().ForEach(x =>
+{
+    string message = $"Message{x}";
+    var messageBody = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+    Console.WriteLine($"Mesaj gönderildi: {message}");
+});
+Console.ReadLine();
---
+++ RabbitMQ.subscriber/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory()
+{
+    Uri = new Uri("amqps://avlbkmsz:VczzGlHupMoF7ximnepb8fKOx4cieMn0@woodpecker.rmq.cloudamqp.com/avlbkmsz")
+};
+//factory.Uri = new Uri("amqps://avlbkmsz:VczzGlHupMoF7ximnepb8fKOx4cieMn0@woodpecker.rmq.cloudamqp.com/avlbkmsz");
+
+using var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+//exclure eğer true yaparsak saece bu kanal üzerinden ulaşabilirz. Ama biz başka processlerden ulaşmmak için false deriz
+//kıuyruk var olduğu için hata olmaz.Burda yazamasak da olur zaten kuyruk publisher da oluştu
+//channel.QueueDeclare("hello-queue", true, false);
+
+channel.BasicQos(0,1,false);
+var consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume("hello-queue", false, consumer);
+consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+    Thread.Sleep(1500);
+    Console.WriteLine("GelenMessage :" + message);
+    channel.BasicAck(e.DeliveryTag, false);
+};
+
+
+
+Console.ReadLine();
